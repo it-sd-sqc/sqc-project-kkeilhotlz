312b8a79f25b49e275dd58f0b947404a589c32bc
kkeilholtz <143773724+kkeilholtz@users.noreply.github.com>
2023-09-25 17:02:07 -0500

    created the .ejs files

--- server.js
+++ server.js
@@ -1,13 +1,51 @@
+// Dependencies ////////////////////////////////////////////
+import 'dotenv/config'
 import express from 'express'
+import pkg from 'pg'
+const { Pool } = pkg
 
+// Configuration ///////////////////////////////////////////
 const PORT = process.env.PORT || 5163
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
-const app = express()
-
-app.use(express.static('./public'))
-
-const displayPort = function () {
-  console.log('Listening on ' + PORT)
+// Query functions /////////////////////////////////////////
+export const query = async function (sql, params) {
+  let client
+  let results = []
+  try {
+    client = await pool.connect()
+    const response = client.query(sql, params)
+    if (response && response.rows) {
+      results = response.rows
+    }
+  } catch (err) {
+    console.error(err)
+  }
+  if (client) client.release()
+  return results
 }
 
-app.listen(PORT, displayPort)
+// const app = express()
+
+// app.use(express.static('./public'))
+
+// const displayPort = function () {
+//   console.log('Listening on ' + PORT)
+// }
+
+// app.listen(PORT, displayPort)
+
+.get('/', function (req, res) {
+  res.render('pages/index')
+})
+
+.get('/about', function (req, res) {
+  res.render('pages/about', { title: 'About' })
+})
+
+.listen(PORT, () => console.log(`Listening on ${PORT}`))
